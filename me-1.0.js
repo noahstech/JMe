72a3b5738ce0e95b1a1d8fa6c152701802890f09
kongxh1218 <304854360@qq.com>
2014-12-29 20:43:01 +0800

    dsafsdaf

--- me-1.0.js
+++ me-1.0.js
@@ -1,17 +1,11 @@
-// version 0.1
-var me = $m = (function (config) {
+var me = (function (config) {
 	var that;
 	var obj = function () {
 		that = this;
 
 		this.pageList = [];
-		this.config = config || {
-			main: "", // 默认打开的页面
-			container: "body", // 根元素
-			hideSelector: "", // 当showType = 1 打开页面的时候，需要隐藏的元素，返回到一级页面时，会重新显示
-			tplPath: "tpl/" // 模板所在的路径，默认为tpl/
-		};
-		this.ctrlExtend = [];
+		this._readyFnList = [];
+		//this._serviceList = [];
 	};
 
 	obj.prototype = {
@@ -20,29 +14,63 @@ var me = $m = (function (config) {
 			that.$compile = $compile;
 			that.$location = $location;
 
+			that._triggerReadyFn();
 			$rootScope.$on('$locationChangeSuccess', that._pageChange);
 
 			that.$scope.show = that.show;
 			that.$scope.hide = that.hide;
 
 			that._init();
-			that.onload && that.onload();
 		},
 
+		// 配置me
+		config: function (cf) {
+			that.config = cf || {
+				main: "", // 默认打开的页面
+				container: "body", // 根元素
+				hideSelector: "", // 当showType = 1 打开页面的时候，需要隐藏的元素，返回到一级页面时，会重新显示
+				tplPath: "tpl/" // 模板所在的路径，默认为tpl/
+			};
+		},
+
+		// 自定义服务
+		//service: function (svName, fn) {
+		//	that._serviceList.push({
+		//		name: svName,
+		//		fn: fn
+		//	});
+		//},
+
+		// 启动angular
+		run: function (appName, plugins) {
+			that._module = angular.module(appName, plugins);
+
+			//for (var i = 0; i < that._serviceList.length; i++) {
+			//	that._module.factory(that._serviceList[i].name, that._serviceList[i].fn);
+			//}
+		},
+
+		// 注入me稳定后需要执行的函数
+		ready: function (fn) {
+			if (typeof fn != "function") return;
+
+			that._readyFnList.push(fn);
+		},
+
+		// 显示一个页面
 		show: function (src, options) {
 			options = options || {
 				showType: 0, // 0：一级页面 1：非一级页面
 				param: {}, // 传递的参数参数
 				title: "", // 页面标题
-				notNgEvent: false, // 是否不是ng事件
+				refresh: false, // 是否立即渲染ui
+				//notNgEvent: false, // 是否不是ng事件
 				onShowed: function () { }, // 页面show成功之后的事件
 				onHided: function () { }, // 页面被隐藏时的事件
 				style: "", // null: 填充（默认） 'pop'：弹出层
 			};
-			console.log("************");
-			console.log(src);
-			console.log(options.param);
-			console.log("************");
+
+			that._log(src, options);
 
 			if (options.title) {
 				document.title = options.title;
@@ -50,16 +78,17 @@ var me = $m = (function (config) {
 				options.title = document.title;
 			}
 
-			var lastPage = that.getLastPage();
-			var html = that._getPageHtml(src, options),
-                container = $(that._getContainer());
+			var lastPage = that._getLastPage(),
+				html = that._getPageHtml(src, options),
+                container = $(that._getContainer()),
+				newPage = that._getLastPage();
+
 			if (options.showType == 0) {
 				container.html(html);
 				that.config.hideSelector && $(that.config.hideSelector).show();
 			} else {
 				lastPage && (lastPage.scrollTop = $(document).scrollTop());
 
-				var newPage = that.getLastPage();
 				if (newPage.style == "pop") {
 					container.append(html);
 				} else {
@@ -71,27 +100,41 @@ var me = $m = (function (config) {
 				that.config.hideSelector && $(that.config.hideSelector).hide();
 			}
 
-			options.notNgEvent && that.$scope.$apply();
+			options.refresh && that.$scope.$apply();
 			options.onShowed && options.onShowed();
+
+			return newPage;
 		},
 
+		// 隐藏页面
 		hide: function () {
 			history.go(-1);
 		},
 
+		// 获得顶层页面的参数
 		param: function () {
-			return that.pageList[that.pageList.length - 1].param;
+			return that.top().param;
 		},
 
-		extendCtrl: function(fnObj) {
-			that.ctrlExtend.push(fnObj);
+		// 执行顶层页面的回调
+		trigger: function () {
+			var page = that.top();
+			page.exec.apply(page, arguments);
 		},
 
-		getLastPage: function (isRemove) {
-			if (that.pageList.length > 0) {
-				return isRemove ? that.pageList.pop() : that.pageList[that.pageList.length - 1];
-			}
+		// 获取顶层的页面对象
+		top: function () {
+			return that._getLastPage();
 		},
+
+		// 定义controller
+		define: function (ctrlName, fn) {
+			that.require(ctrlName, fn);
+		},
+
+		//extendCtrl: function (fnObj) {
+		//	that.ctrlExtend.push(fnObj);
+		//},
 
 		require: function (ctrlName, fn) {
 			if (window[ctrlName]) throw new Error("me中已经注册了名为" + ctrlName + "的控制器");
@@ -130,18 +173,24 @@ var me = $m = (function (config) {
 					}
 				}
 
-				for (var i = 0; i < that.ctrlExtend.length; i++) {
-					for (var j in that.ctrlExtend[i]){
-						pro[j] = that.ctrlExtend[i][j];
-					}
-				}
+				//for (var i = 0; i < that.ctrlExtend.length; i++) {
+				//	for (var j in that.ctrlExtend[i]) {
+				//		pro[j] = that.ctrlExtend[i][j];
+				//	}
+				//}
 
 				obj.prototype = pro;
 
 				return new obj();
 			})();
-			
+
 			return window[ctrlName];
+		},
+
+		_getLastPage: function (isRemove) {
+			if (that.pageList.length > 0) {
+				return isRemove ? that.pageList.pop() : that.pageList[that.pageList.length - 1];
+			}
 		},
 
 		_hidePage: function () {
@@ -150,15 +199,15 @@ var me = $m = (function (config) {
 			}
 
 			// 删掉当前页面
-			var curPageObj = that.getLastPage(true);
+			var curPageObj = that._getLastPage(true);
 			that._cleanCtrl();
 			curPageObj.onHided && curPageObj.onHided();
 			$("#" + curPageObj.id).remove();
 
 			// 显示上一个页面
-			var lastPage = that.getLastPage();
-			
-			if(lastPage) {
+			var lastPage = that._getLastPage();
+
+			if (lastPage) {
 				that.$location.hash(lastPage.hash);
 				$("#" + lastPage.id).show();
 				document.title = lastPage.title;
@@ -167,7 +216,7 @@ var me = $m = (function (config) {
 					window.scrollTo(0, lastPage.scrollTop);
 				});
 			}
-			
+
 			if (that.config.hideSelector && that.pageList.length == 1) {
 				$(that.config.hideSelector).show();
 			}
@@ -194,7 +243,7 @@ var me = $m = (function (config) {
 		},
 
 		_pageChange: function (angularEvent, newUrl, oldUrl) {
-			var lastPage = that.getLastPage();
+			var lastPage = that._getLastPage();
 
 			if (!lastPage) {
 				return;
@@ -238,6 +287,8 @@ var me = $m = (function (config) {
 				src: src
 			};
 
+			that._appendEvent(pageObj);
+
 			if (options.showType == 0) {
 				that._cleanCtrl(true);
 				that.pageList = [pageObj];
@@ -246,6 +297,33 @@ var me = $m = (function (config) {
 			}
 
 			return page;
+		},
+
+		_appendEvent: function (pageObj) {
+			pageObj.on = function (ename, callback) {
+				if (typeof (callback) != "function") return;
+				this._eventMap = this._eventMap || {};
+				this._eventMap[ename] = callback;
+				return this;
+			}
+
+			pageObj.exec = function (ename) {
+				if (!ename
+					|| !this._eventMap
+					|| !(ename in this._eventMap)
+					|| typeof (this._eventMap[ename]) != "function")
+					return;
+
+				var arg = null;
+				if (arguments.length > 1) {
+					arg = [];
+					for (var i = 1; i < arguments.length; i++) {
+						arg.push(arguments[i]);
+					}
+				}
+				this._eventMap[ename].apply(this, arg);
+				return this;
+			}
 		},
 
 		_getContainer: function () {
@@ -257,8 +335,24 @@ var me = $m = (function (config) {
 			var r = window.location.search.substr(1).match(reg);
 			if (r != null) return unescape(r[2]);
 			return "";
+		},
+
+		_log: function (src, options) {
+			console.group("me.js", "");
+			console.log("%c 链接：" + src, "color:green");
+			console.log("%c 参数：" + (options.param ? "" : "[无]"), "color:green");
+			options.param && console.log(options.param)
+			console.groupEnd();
+		},
+
+		_triggerReadyFn: function () {
+			if (that._readyFnList.length == 0) return;
+
+			for (var i = 0; i < that._readyFnList.length; i++) {
+				that._readyFnList[i](that.$scope);
+			}
 		}
 	};
 
 	return new obj();
-})(window.Config);
+})();
